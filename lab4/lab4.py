2d84fefa8cc1042a06f67416ec9c711fbb3c12db
Ashlyn Thill <ashlynthill@mail.weber.edu>
2019-02-25 15:30:12 -0700

    Created time_elapsed method

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -50,7 +50,7 @@ def read_gps_data(gps_file, harbor_data):
         for line in file:
             line_words = line.split()       #separate entries by space
             gps_times.append("{0}:{1}:{2}".format(line_words[0], line_words[1], line_words[2]))    #add times to gps_times list
-            gps_altitude.append(line_words[6])  #add altitude to gps_altitude list
+            gps_altitude.append(int(line_words[6]))  #add altitude to gps_altitude list
     harbor_data["gps_times"] = gps_times
     harbor_data["gps_altitude"] = gps_altitude
     
@@ -68,7 +68,24 @@ def interpolate_wx_from_gps(harbor_data):
     :param harbor_data: A dictionary to collect data.
     :return: Nothing
     """
-    pass
+    alts = harbor_data["gps_altitude"]
+    temps = harbor_data["wx_temperatures"]
+    wx_alt_up = [alts[0]]     #list of altitudes for ascent
+    wx_temp_up = [temps[0]]        #list of temps for ascent
+    wx_alt_down = []                                    #list of altitudes for descent
+    wx_temp_down = []                                   #list of temps for descent
+
+    index = 1
+    while [alts[index]] >= [wx_alt_up[index-1]]:        #run while the altitude is increasing
+        wx_alt_up += [alts[index]]
+        wx_temp_up += [temps[index]]
+        index+=1
+    while index < (len(alts)):      #collect the rest of the altitudes (descent)
+        wx_alt_down += [alts[index]]
+        index+=1
+
+
+
 
 
 def plot_figs(harbor_data):
@@ -78,6 +95,25 @@ def plot_figs(harbor_data):
     :return: nothing
     """
     pass
+
+def time_elapsed(times, title, harbor_data):
+    """
+    Turn HH:MM:SS times into list of elapsed time, start at 0
+    :param times: list of times in HH:MM:SS format
+    :param harbor_data: dictionary to collect data
+    :return: nothing
+    """
+    elapsed_times = [0]
+    start_time = times[0]
+    start_time = int((int(start_time[0:2]) * 3600) + (int(start_time[3:5]) * 60) + int(start_time[6:8]))
+    for time in times:
+        split_time = time.split(':')
+        hours = int(split_time[0]) * 3600
+        minutes = int(split_time[1]) * 60
+        seconds = int(split_time[2])
+        elapsed_times.append((hours + minutes + seconds) - start_time)
+    harbor_data[title] = elapsed_times
+
 
 
 def main():
@@ -90,9 +126,11 @@ def main():
     gps_file = sys.argv[2]                  # second program input param
 
     read_wx_data(wx_file, harbor_data)      # collect weather data
+    time_elapsed(harbor_data["wx_times"], "wx_times", harbor_data)
+    print(harbor_data["wx_times"])
     read_gps_data(gps_file, harbor_data)    # collect gps data
-        
-    pp.pprint(harbor_data["gps_times"])
+    time_elapsed(harbor_data["gps_times"], "gps_times", harbor_data)
+    print(harbor_data["gps_times"])
     interpolate_wx_from_gps(harbor_data)    # calculate interpolated data
     plot_figs(harbor_data)                  # display figures
 
