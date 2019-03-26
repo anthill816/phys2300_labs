7a9f8bba792a32fb6e0a157aeb502bad4af2a93e
Ashlyn Thill <ashlynthill@mail.weber.edu>
2019-03-26 17:28:10 -0600

    Tasks 1-3 completed

--- lab6/pendulum_template.py
+++ lab6/pendulum_template.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from vpython import *
 
 g = 9.81    # m/s**2
-l = 0.1     # meters
+l = 1    # meters
 W = 0.002   # arm radius
 R = 0.01     # ball radius
 framerate = 100
@@ -13,7 +13,7 @@ def set_scene():
     """
     Set Vpython Scene
     """
-    scene.title = "Assignment 5: Projectile motion"
+    scene.title = "Assignment 6: Not so simple pendulum"
     scene.width = 800
     scene.heigth = 600
     scene.caption = """Right button drag or Ctrl-drag to rotate "camera" to view scene.
@@ -24,22 +24,37 @@ def set_scene():
     scene.x = -1
     # Set background: floor, table, etc
 
-def f(r):
+def f(angles):
     """
     Pendulum
     """
-    theta = r[0]
-    omega = r[1]
+    theta = angles[0]
+    omega = angles[1]
     ftheta = omega
-    fomega = -(g/l)*np.sin(theta)
+    fomega = -(g/l)*np.sin(theta) -  2 * omega
     return np.array([ftheta, fomega], float)
+
+def plot_theta(angles, times):
+    """
+    Create of plot of theta v time
+    Param: angles = list of thetas
+    Param: times = list of times
+    """
+    plt.plot(times, angles)
+    plt.show()
+    
 
 
 def main():
     """
+    Main method
     """
+    #set vpython scene
     set_scene()
-    print("hi")
+
+    angles = []
+    times = []
+
     # Set up initial values
     h = 1.0/(framerate * steps_per_frame)
     r = np.array([np.pi*179/180, 0], float)
@@ -48,25 +63,32 @@ def main():
     y = -l*np.cos(r[0])
 
     #display starting point of pendulum rod
-    pivotball = sphere()
-    rod = cylinder(pos=vector(x, y, 0), axis=vector(1, 0, 0), radius=.01)
+    pivotball = sphere(pos=vector(x, y, 0), radius=.05)
+    rod = cylinder(pos=vector(x, y, 0), axis=vector(cos(r[0]), -sin(r[0]), 0), radius=.01)
     # Loop over some time interval
     dt = 0.01
     t = 0
         # Use the 4'th order Runga-Kutta approximation
-    for i in range(steps_per_frame):
-        k1 = h * f([angles, t])
-        k2 = h * f([angles + 0.5 * k1, t])
-        k3 = h * f([angles + 0.5 * k2, t])
-        k4 = h * f([angles + k3, t])
-        angles += (k1 + 2 * k2, 2 * k3, k4)/6
-        r += h*f(r)
+    for i in range(framerate):
+        # k1 = h * f([r, t])
+        rate(5)
+        k1 = f(r)
+        k2 = h * f(r + 0.5 * k1)
+        k3 = h * f(r + 0.5 * k2)
+        k4 = h * f(r + k3)
+        r += (k1 + 2 * k2 + 2 * k3 + k4)/6
+        # r += h*f(r)
+        angles.append(r[0])
+        times.append(t)
         t += dt
         # Update positions
-        x = l*np.sin(r[0])
-        y = -l*np.cos(r[0])
+        x = np.cos(r[0] - np.pi/2)
+        y = -abs(np.sin(r[0] - np.pi/2)) 
+        
         # Update the cylinder axis
+        rod.axis = vector(x, y, 0)
         # Update the pendulum's bob
+    plot_theta(angles, times)
 
 if __name__ == "__main__":
     main()
